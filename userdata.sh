c48c9873e0450cf755a1352e8e2a6446b57a1339
saipranayteja <pranayteja.ch@algonox.com>
2023-06-21 00:43:08 +0530

    sdf

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo labauto ansible
-sudo ansible-pull -i localhost, -U https://github.com/sai-pranay-teja/roboshop-ansible.git roboshop-app.yml -e components=${component} >/opt/ansible.log
+sudo ansible-pull -i localhost, -U https://github.com/sai-pranay-teja/roboshop-ansible.git roboshop-app.yml -e env=dev -e components=${component} >/opt/ansible.log
