dd0e9db9cd4fde4d585cac4dd923afc628a8a9fb
saipranayteja <pranayteja.ch@algonox.com>
2023-07-10 17:51:04 +0530

    sdcf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-
+/* 
 resource "aws_route53_record" "component-records" {
     zone_id = data.aws_route53_zone.mine.zone_id
     name    = "rabbitmq-${var.env}.practise-devops.online"
@@ -73,8 +73,61 @@ resource "aws_security_group" "rabbitmq-traffic" {
     Name = "${var.env}-Rabbitmq traffic"
   }
 }
+ */
+
+resource "aws_route53_record" "component-records" {
+    zone_id = data.aws_route53_zone.mine.zone_id
+    name    = "rabbitmq-${var.env}.practise-devops.online"
+    type    = "A"
+    ttl     = 30
+    records = [aws_instance.rabbitmq.private_ip]
+}
 
 
 
+resource "aws_instance" "rabbitmq" {
+  ami           = data.aws_ami.centos-ami.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ aws_security_group.rabbitmq-traffic.id ]
+  subnet_id = var.subnet_ids[0]
+  iam_instance_profile = aws_iam_instance_profile.Full-access-profile.name
+  tags = {
+        Name = "${var.env}-rabbitmq-server"
+    }
 
 
+} 
+
+resource "aws_security_group" "rabbitmq-traffic" {
+  name        = "Rabbitmq traffic"
+  description = "Rabbitmq traffic"
+  vpc_id=var.vpc_id
+
+
+  ingress {
+    description      = "RabbitMQ Traffic"
+    from_port        = 5672
+    to_port          = 5672
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_subnets
+  }
+
+  ingress {
+    description      = "SSH Traffic"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [data.aws_vpc.default.cidr_block]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-Rabbitmq traffic"
+  }
+}
